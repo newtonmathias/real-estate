421f4ac2f1aaf5a0bd01046d67c890a76f21ca23
newton mathias <newtymathias@gmail.com>
2023-10-13 21:41:07 +0300

    middlewares

--- api/index.js
+++ api/index.js
@@ -1,13 +1,14 @@
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
-
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
-    console.log("connected to the db");
+    console.log("Connected to the db");
   })
   .catch((err) => {
     console.log(err);
@@ -15,6 +16,21 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("server is running on port 3000");
+});
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
